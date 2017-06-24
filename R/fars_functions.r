9863cdd9968e98df7307515c46e73fcbc82d28f6
Joseph Harkness <jharkness@loe.corp>
2017-06-24 14:27:54 -0400

    adding test

--- R/fars_functions.r
+++ R/fars_functions.r
@@ -1,17 +1,17 @@
 #'  Read FARS data
 #'
-#'  Creates a dataframe from csv formatted file of data from the US National Highway Traffic Safety Administration's 
-#'  Fatality Analysis #'  Reporting System (FARS), which is a nationwide census providing the American public yearly 
+#'  Creates a dataframe from csv formatted file of data from the US National Highway Traffic Safety Administration's
+#'  Fatality Analysis #'  Reporting System (FARS), which is a nationwide census providing the American public yearly
 #'  data regarding fatal injuries suffered in motor vehicle traffic crashes.  If full file path is not provided, this
-#'  function will look in the current working directory for the file 
+#'  function will look in the current working directory for the file
 #'
 #' @param filename Name of csv file with FARS data
-#' 
+#'
 #' @return This function returns a dataframe with FARS data if the file exists
 #'
 #' @importFrom dplyr tbl_df
 #' @importFrom readr read_csv
-#' @examples 
+#' @examples
 #' \dontrun{fars_data <- fars_read(accident_2013.csv.bz2)}
 #'
 #' @export
@@ -29,7 +29,7 @@ fars_read <- function(filename) {
 #'
 #' @param year Year of FARS input data to read
 #' @return A FARS data filename for a specified year
-#' @examples 
+#' @examples
 #' \dontrun{fars_fileame <- make_filename(2013)}
 #'
 #' @export
@@ -41,15 +41,15 @@ make_filename <- function(year) {
 
 #' Creates a dataframe from FARS data from multiple years as specified by the /code{years} parameter
 #'
-#' This function also requires the %>% function from the package dplyr, so you that package must be loaded 
+#' This function also requires the %>% function from the package dplyr, so you that package must be loaded
 #' for the function to work.  (It seems like there should be a tag to indicate things like this, ditto with map below)
-#' 
+#'
 #' @param years Vector of Years of FARS input data to read
 #' @return A FARS dataframe for the specified year
-#' 
+#'
 #' @importFrom dplyr mutate
 #' @importFrom dplyr select
-#' @examples 
+#' @examples
 #' \dontrun{fars_data <- fars_read_years(c(2013, 2014, 2015)}
 #'
 #' @export
@@ -59,7 +59,7 @@ fars_read_years <- function(years) {
                 file <- make_filename(year)
                 tryCatch({
                         dat <- fars_read(file)
-                        dplyr::mutate(dat, year = year) %>% 
+                        dplyr::mutate(dat, year = year) %>%
                                 dplyr::select(MONTH, year)
                 }, error = function(e) {
                         warning("invalid year: ", year)
@@ -68,50 +68,50 @@ fars_read_years <- function(years) {
         })
 }
 
-#' Creates a dataframe summarizing the number of observations by in FARS data from multiple years as specified by the 
+#' Creates a dataframe summarizing the number of observations by in FARS data from multiple years as specified by the
 #' /code{years} parameter
 #'
 #' @param years Vector of Years of FARS input data to read
 #' @return A dataframe summarizing the number of FARS data observations by year for the specified years
-#' 
+#'
 #' @importFrom dplyr bind_rows
 #' @importFrom dplyr group_by
 #' @importFrom dplyr summarize
 #' @importFrom tidyr spread
-#' 
-#' @examples 
+#'
+#' @examples
 #' \dontrun{fars_summary <- fars_summarize_years(c(2013, 2014, 2015)}
 #'
 #' @export
 
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
-        dplyr::bind_rows(dat_list) %>% 
-                dplyr::group_by(year, MONTH) %>% 
+        dplyr::bind_rows(dat_list) %>%
+                dplyr::group_by(year, MONTH) %>%
                 dplyr::summarize(n = n()) %>%
                 tidyr::spread(year, n)
 }
 
-#' Creates a map of accident locations for one as specified by the /code{year} parameter for the 
+#' Creates a map of accident locations for one as specified by the /code{year} parameter for the
 #' state specified by FIPS code using the /code{state.num} parameter.
-#' 
+#'
 #' This function requires the package map to be loaded or if map is installed to run the assignment
 #' stateMapEnv <- "R_MAP_DATA_DIR".  I don't know how to properly include this warning in the documentation.
 #'
 #' @param year Year of FARS input data to use
 #' @param state.num FIPS code of the state to create a map for
-#' @return A map of accident locations for state with FIPS code /code{state.num}  in year /code{year}. 
-#' 
+#' @return A map of accident locations for state with FIPS code /code{state.num}  in year /code{year}.
+#'
 #' @importFrom dplyr filter
 #' @importFrom map map
 #' @importFrom graphics points
-#' 
-#' @examples 
+#'
+#' @examples
 #' \dontrun{fars_map_state(24,2013)}
 #'
 #' @export
 
-library(map)
+library(maps)
 fars_map_state <- function(state.num, year) {
         filename <- make_filename(year)
         data <- fars_read(filename)
---
+++ tests/test1.R
@@ -0,0 +1,2 @@
+library(testthat)
+expect_that(make_filename(2012),matches("accident_2012.csv.bz2"))
